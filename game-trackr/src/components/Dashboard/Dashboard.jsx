8431e163865413bb784ed4321fb61e56fd3d717c
Sergio G <sergio.garcia.cristobal@gmail.com>
2024-08-29 12:21:05 +0200

    Upload covers

--- game-trackr/src/components/Dashboard/Dashboard.jsx
+++ game-trackr/src/components/Dashboard/Dashboard.jsx
@@ -2,9 +2,9 @@ import { useRef } from 'react';
 import './Dashboard.css';
 
 export default function Dashboard({ games, onUpdateGameCover }) {
-    const imgPlaceholder = 'public/placeholder-img.jpg'
+    const imgPlaceholder = 'public/placeholder-img.jpg';
 
-    const fileInputRef = useRef(null);
+    const fileInputRefs = useRef({});
 
     const handleFileChange = (event, gameId) => {
         const file = event.target.files[0];
@@ -17,35 +17,32 @@ export default function Dashboard({ games, onUpdateGameCover }) {
         }
     };
 
-    const handleImageClick = (event) => {
-
-        fileInputRef.current.click();
+    const handleImageClick = (gameId) => {
+        fileInputRefs.current[gameId].click();
     };
 
     return (
         <div className='dashboard'>
             <ul className='game-list'>
                 {games.map(game => (
-                    
                     <li key={game.id} className='game-card'>
                         <img
                             src={game.coverImage || imgPlaceholder}
                             alt={`${game.name} cover`}
                             className='game-card-img'
-                            onClick={handleImageClick}
+                            onClick={() => handleImageClick(game.id)}
                         />
-
                         <div className="game-details">
                             <h2>{game.name}</h2>
                             <p>Progress: {game.progress}%</p>
                         </div>
-
                         <input
                             type="file"
                             accept="image/*"
                             onChange={(event) => handleFileChange(event, game.id)}
                             className='upload-input'
-                            ref={fileInputRef}
+                            ref={(el) => (fileInputRefs.current[game.id] = el)}
+                            style={{ display: 'none' }}
                         />
                     </li>
                 ))}
